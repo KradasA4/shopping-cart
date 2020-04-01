e2ef65cd7b89bc49d96778707154d021277952e5
KradasA4 <wichai.sw19@gmail.com>
2020-04-01 16:47:36 +0700

    add AntD menu

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import CategoryMenu from './components/Category/CategoryMenu';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <CategoryMenu />
     </div>
   );
 }
---
+++ src/components/Category/CategoryMenu.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Menu, Button } from 'antd';
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined,
+} from '@ant-design/icons';
+
+class CategoryMenu extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+
+
+  render() {
+    return (
+      <div style={{ width: 256 }}>
+        {/* <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+        </Button> */}
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          // inlineCollapsed={this.state.collapsed}
+        >
+          <Menu.Item key="1">
+            <PieChartOutlined />
+            <span>Burger</span>
+          </Menu.Item>
+          <Menu.Item key="2">
+            <DesktopOutlined />
+            <span>Side Dish</span>
+          </Menu.Item>
+          <Menu.Item key="3">
+            <ContainerOutlined />
+            <span>Dessert</span>
+          </Menu.Item>
+          <Menu.Item key="4">
+            <ContainerOutlined />
+            <span>Beverage</span>
+          </Menu.Item>
+        </Menu>
+      </div>
+    )
+  }
+
+
+}
+
+export default CategoryMenu;
