8bf3fadb415596b6104a71d5a5350b7f5b4c1d26
KradasA4 <wichai.sw19@gmail.com>
2020-04-03 01:12:27 +0700

    add onClikcAddtoCart to pass it from ProductCard to App
    this act as a middlemen to pass the props

--- src/components/ProductDisplay/ProductDisplay.js
+++ src/components/ProductDisplay/ProductDisplay.js
@@ -3,23 +3,23 @@ import { Row } from 'antd';
 import ProductCard from './ProductCard';
 
 export default class ProductDisplay extends Component {
+
   render() {
-    // const { id, product, category, price, image, description } = this.props
-    const products = this.props.products
-    const productArray = products.map( (item, index) => {   
+    const { products, onClickAddToCart } = this.props
+    
+    const productArray = products.map( (item) => {
       return (
         <ProductCard 
-          id={products[index].id}
-          name={products[index].name}
-          category={products[index].category}
-          price={products[index].price}
-          image={products[index].image}
-          description={products[index].description}
+          id={item.id}
+          name={item.name}
+          category={item.category}
+          price={item.price}
+          image={item.image}
+          description={item.description}
+          onClickAddToCart={onClickAddToCart}
         />
       )
     });
-    
-    console.log("ProductDisplay")
     
     return (
       <Row>
