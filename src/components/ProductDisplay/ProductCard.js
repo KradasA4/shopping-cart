d05225f9bcd726de40a647388ea374cd75d291f9
KradasA4 <wichai.sw19@gmail.com>
2020-04-03 01:07:55 +0700

    add onClickAddToCart function from props
    this props let App.js has access to e.target.id

--- src/components/ProductDisplay/ProductCard.js
+++ src/components/ProductDisplay/ProductCard.js
@@ -6,10 +6,9 @@ import './ProductCard.css';
 const { Meta } = Card;
 
 export default class ProductCard extends Component {
+  
   render() {
     const {id, name, category, price, image, description} = this.props;
-
-    console.log("Product Card")
 
     return (
       <div>
@@ -20,7 +19,16 @@ export default class ProductCard extends Component {
           className="antd__Card"
         >
           <Meta title={name} description={description} />
-          <Button type="primary" className="Button--center" justify="center">Add</Button>
+
+          <Button 
+            type="primary" 
+            className="Button--center" 
+            justify="center" 
+            id={id}
+            onClick={ (e) => this.props.onClickAddToCart(e.target.id) }
+          >
+            Add
+          </Button>
         </Card>
       </div>
     )
