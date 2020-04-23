3e60d21a926ea37fa4c68c55171a521c7c833e89
KradasA4 <wichai.sw19@gmail.com>
2020-04-24 01:28:32 +0700

    Updates

---
+++ precache-manifest.a094d76e3e28f9cb58d7be5b8af9dd95.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3c75094e73550efb43afabef2ae4a1f",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "44afbc60070dffad5be5",
+    "url": "/shopping-cart/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "eb56bb95e36ba32f0ff0",
+    "url": "/shopping-cart/static/css/main.24eaa670.chunk.css"
+  },
+  {
+    "revision": "44afbc60070dffad5be5",
+    "url": "/shopping-cart/static/js/2.1824a52f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/shopping-cart/static/js/2.1824a52f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb56bb95e36ba32f0ff0",
+    "url": "/shopping-cart/static/js/main.170ef66f.chunk.js"
+  },
+  {
+    "revision": "5c2abeca3ad336da602d",
+    "url": "/shopping-cart/static/js/runtime-main.8330f80e.js"
+  },
+  {
+    "revision": "aa699e40ca34ef0b0c33c16d7d84f305",
+    "url": "/shopping-cart/static/media/cake.aa699e40.jpg"
+  },
+  {
+    "revision": "bb2ed30c50377690b48d287aedbfc7e3",
+    "url": "/shopping-cart/static/media/cheese-burger.bb2ed30c.jpg"
+  },
+  {
+    "revision": "0c0ab88b5e7a5dc47edbb7d815b52fe8",
+    "url": "/shopping-cart/static/media/chicken-wings.0c0ab88b.jpg"
+  },
+  {
+    "revision": "1e8223c5f805ca175a6f5910e0176fe1",
+    "url": "/shopping-cart/static/media/coca-cola.1e8223c5.jpg"
+  },
+  {
+    "revision": "9443fa2d3d5d23165d2d9bcb9c91cd61",
+    "url": "/shopping-cart/static/media/fries.9443fa2d.jpg"
+  },
+  {
+    "revision": "fdc00a34f6c06a70a9f3da628743b106",
+    "url": "/shopping-cart/static/media/ice-cream.fdc00a34.jpg"
+  },
+  {
+    "revision": "9dd6c6b2b7d3f1ab6c10ee26d2778089",
+    "url": "/shopping-cart/static/media/mashed-potato.9dd6c6b2.jpg"
+  },
+  {
+    "revision": "b368aba6d124f5cad684f7b77b8fa776",
+    "url": "/shopping-cart/static/media/orange-jucie.b368aba6.jpg"
+  },
+  {
+    "revision": "b0d71a967ae76d2b1edd9ebe2bfa4eb5",
+    "url": "/shopping-cart/static/media/signature-burger.b0d71a96.jpg"
+  }
+]);
